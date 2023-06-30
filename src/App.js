7ec54d3f70c3a948d7169fd454e700fb52089303
Moises Baldenegro Melendez <moisesm16@gmail.com>
2023-06-29 18:11:19 -0600

    sesion de estilos tres

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import characters from './data.js';
 function App() {
    return (
       <div className={style.App}>
-         <header className={style.header}><SearchBar onSearch={(characterID) => window.alert(characterID)} /></header>
+         <header className={style.header}>
+            <img className={style.headerImg}  src="./assets/img/Rick-And-Morty-Logo.png" alt=""></img>
+            
+         </header>
+         <section className={style.search}>
+            <h1> Can we help?</h1>
+            <SearchBar onSearch={(characterID) => window.alert(characterID)} />
+         </section>
          <aside className={style.aside}></aside>
          <main className={style.main}><Cards characters={characters} /></main>
-         <footer className={style.footer}>FOOTER</footer>
-      
-         
-         
+         <footer className={style.footer}>FOOTER</footer> 
       </div>
 
    );
